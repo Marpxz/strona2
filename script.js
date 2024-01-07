09b00521664c66cd77b5d62781dca05edb370329
Marpxz <martynapyzio2@gmail.com>
2024-01-07 18:55:21 +0100

    galeria

---
+++ script.js
@@ -0,0 +1,23 @@
+// Skrypt JavaScript do obsługi galerii zdjęć
+
+// Pobierz wszystkie obrazy w galerii
+const images = document.querySelectorAll('.gallery img');
+
+// Przypisz funkcję do każdego obrazka, która powiększy obraz po kliknięciu
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        // Stwórz nowy element obrazka
+        const enlargedImage = document.createElement('img');
+        enlargedImage.src = this.src;
+        enlargedImage.alt = this.alt;
+        enlargedImage.classList.add('enlarged');
+
+        // Dodaj powiększony obraz do body
+        document.body.appendChild(enlargedImage);
+
+        // Funkcja do zamknięcia powiększonego obrazu po kliknięciu
+        enlargedImage.addEventListener('click', function() {
+            document.body.removeChild(this);
+        });
+    });
+});
